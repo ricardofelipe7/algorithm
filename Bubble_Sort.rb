58ebedfef046e0fa64c327611bc3da8d4d470151
ricardofelipe7 <rikrdoofelipe@outlook.com>
2023-03-02 17:00:08 -0800

    cod bubbleSort create.

---
+++ Bubble_Sort.rb
@@ -0,0 +1,30 @@
+class AlgorithmsSorting
+    
+    def bubble_sort(array)
+        length = array.length
+        return array if length <= 1
+
+        loop do
+            swapped = false
+
+            (length -1). times do |i|
+                next unless array[i] > array[i + 1]
+
+                exp = array[i]
+                array[i] = array[i + 1]
+                array[i + 1] = exp
+
+                swapped = true
+            end
+
+            break unless swapped
+        end
+
+        array
+    end
+
+end
+
+array = [64, 34, 25, 12, 22, 11, 90]
+
+p AlgorithmsSorting.new.bubble_sort(array)
