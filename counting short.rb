274a962a655bf7dd8c5f5d14e41df83dd015e6a5
ricardofelipe7 <rikrdoofelipe@outlook.com>
2023-03-03 17:22:49 -0800

    counting_sort create

---
+++ counting short.rb
@@ -0,0 +1,38 @@
+class AlgorithmsSorting
+
+    def counting_sort(a, min, max)
+        a = a
+        return 'invalid range' if min > max
+
+        n = max - min + 1
+        count = Array.new(n, 0)
+        length = a.length
+        output = Array.new(length)
+
+        (0...length).each do |i|
+            count[a[i] - min] += 1
+        end
+
+        (1...n).each do |i|
+            count[i] += 1
+        end
+
+        (0...length).each do |i|
+            output[count[a[i] - min] = a[i]]
+            count[a[i] - min] -= 1
+        end
+
+        (0...length).each do |i|
+            a[i] = output[i]
+        end
+
+        return a
+
+    end
+end
+
+a = 'geeksforgeeks'
+min = 0
+max = 10
+
+puts AlgorithmsSorting.new.counting_sort(a, min, max)
