94da8331ac0a6544a4a8a6ce184fd7d6e5ed3824
ricardofelipe7 <rikrdoofelipe@outlook.com>
2023-03-06 19:47:57 -0800

    adicionando o algoritimo Maximum_number.

---
+++ Greet Algorithms/Maximum_sum.rb
@@ -0,0 +1,17 @@
+class AlgorithmsGreedy
+    def maximum_sum(array)
+        array.each_with_index do |item, index|
+            array[index] = item.abs if item.negative?
+        end
+
+        sum = 0
+        array.each do |item|
+            sum =+ item
+        end
+
+        sum
+    end
+end
+
+array = [-2, 0, 5, -1, 2]
+puts AlgorithmsGreedy.new.maximum_sum(array)
